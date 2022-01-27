be8e40b88bfeee02ce8d3db52f558011cf9bb6f2
sznpok <sijanpok55@gmail.com>
2022-01-27 12:09:58 +0545

    created subcategory menu model

--- api/models.py
+++ api/models.py
@@ -16,3 +16,14 @@ class CategoryMenu(models.Model):
 
     def __str__(self):
         return f'{self.menu},{self.foodTypeId.type}'
+
+class SubCategoryMenu(models.Model):
+    foodTypeId = models.ForeignKey(FoodType,on_delete=models.CASCADE)
+    categoryMenuId= models.ForeignKey(CategoryMenu,on_delete=models.CASCADE)
+    menuName = models.CharField(max_length=30,null=True,blank=True)
+    image = models.ImageField(upload_to='images/menuItem')
+    unit = models.CharField(max_length=20,null=True,blank=True)
+    price = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.menuName
