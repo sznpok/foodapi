183233417df8328af459ca3ebf795966f9c49369
sznpok <sijanpok55@gmail.com>
2022-02-03 15:20:24 +0545

    category menu viewset serlizer ,router created

--- api/serializer.py
+++ api/serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from .models import CategoryMenu,FoodType,Orders,SubCategoryMenu,Table,TableType,TimeForOrder
 
-class FoodTypeSeralizer(serializers.ModelSerializer):
+class FoodTypeSerializer(serializers.ModelSerializer):
     type = serializers.CharField(max_length=20)
     class Meta:
         model = FoodType
         fields = "__all__"
     
+
+class CategoryMenuSerializer(serializers.ModelSerializer):
+    menu = serializers.CharField(max_length=30,)
+    class Meta:
+        model = CategoryMenu
+        fields = ['foodTypeId','menu']
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from .models import FoodType
-from .serializer import FoodTypeSeralizer
+from .models import CategoryMenu, FoodType
+from .serializer import CategoryMenuSerializer, FoodTypeSerializer
 from rest_framework import viewsets
 
 from rest_framework.response import Response
@@ -11,11 +11,11 @@ class FoodTypeViewset(viewsets.ViewSet):
     queryset = FoodType.objects.all()
 
     def list(self,request):
-        serializer = FoodTypeSeralizer(self.queryset,many=True)
+        serializer = FoodTypeSerializer(self.queryset,many=True)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
 
     def create(self,request):
-        seralizer = FoodTypeSeralizer(data=request.data)
+        seralizer = FoodTypeSerializer(data=request.data)
         if seralizer.is_valid():
             seralizer.save()
             return Response({
@@ -30,7 +30,7 @@ class FoodTypeViewset(viewsets.ViewSet):
 
     def update(self, request, pk):
         foodType = FoodType.objects.get(pk=pk)
-        serializer = FoodTypeSeralizer(foodType, data=request.data)
+        serializer = FoodTypeSerializer(foodType, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"status":"success","data":serializer.data},status = status.HTTP_201_CREATED)
@@ -42,3 +42,61 @@ class FoodTypeViewset(viewsets.ViewSet):
         foodType = get_object_or_404(FoodType,pk=pk)
         foodType.delete()
         return Response({"status":"success","data":"food type  deleted"})
+
+
+class CategoryMenuviewset(viewsets.ViewSet):
+    queryset = CategoryMenu.objects.all()
+
+    def list(self, request):
+        """
+        This will return list of objects.
+        """
+        serializer = CategoryMenuSerializer(self.queryset, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        
+    def create(self, request):
+        """
+        This will create an endpoint for POST request.
+        """
+        serializer = CategoryMenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":serializer.data},status = status.HTTP_201_CREATED)
+
+        else:
+            return Response({"status":"error","data":serializer.errors},status = status.HTTP_400_BAD_REQUEST)
+        
+ 
+    def retrieve(self, request, pk=None):
+        """
+        Returns a single object
+        """
+        category = get_object_or_404(self.queryset, pk=pk)
+        serializer = CategoryMenuSerializer(category)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        
+ 
+    def update(self, request, pk):
+        category = CategoryMenu.objects.get(pk=pk)
+        serializer = CategoryMenuSerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":serializer.data},status = status.HTTP_201_CREATED)
+
+        else:
+            return Response({"status":"error","data":serializer.errors},status = status.HTTP_400_BAD_REQUEST)
+ 
+    def partial_update(self, request,pk):
+        category = CategoryMenu.objects.get(pk=pk)
+        serializer = CategoryMenuSerializer(category, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":serializer.data},status = status.HTTP_201_CREATED)
+
+        else:
+            return Response({"status":"error","data":serializer.errors},status = status.HTTP_400_BAD_REQUEST)
+ 
+    def destroy(self, request,pk):
+        category = get_object_or_404(CategoryMenu,pk=pk)
+        category.delete()
+        return Response({"status":"success","data":"Item Deleted"})
