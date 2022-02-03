58f2243c4ebf20d8ecfcdb0ebb4b024d20471608
sznpok <sijanpok55@gmail.com>
2022-02-03 16:21:32 +0545

    order ,table ,are created(viewset,router,seralizer)

--- api/serializer.py
+++ api/serializer.py
@@ -5,11 +5,36 @@ class FoodTypeSerializer(serializers.ModelSerializer):
     type = serializers.CharField(max_length=20)
     class Meta:
         model = FoodType
-        fields = "__all__"
+        fields = ["type"]
     
-
 class CategoryMenuSerializer(serializers.ModelSerializer):
     menu = serializers.CharField(max_length=30,)
     class Meta:
         model = CategoryMenu
         fields = ['foodTypeId','menu']
+
+class SubCategoryMenuSerializer(serializers.ModelSerializer):
+
+    menuName = serializers.CharField(max_length=30,)
+    unit = serializers.CharField(max_length=20)
+
+    class Meta:
+        model = SubCategoryMenu
+        fields = "__all__"
+
+class TableTypeSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(max_length=20)
+    class Meta:
+        model = TableType
+        fields = "__all__"
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = ['tableTypeId','tableName','status']
+
+class OrderSerialzer(serializers.ModelSerializer):
+    quantity = serializers.IntegerField()
+    class Meta:
+        model = Orders
+        fields = "__all__"
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,15 @@
 from email.mime import base
 from django.urls import path,include
-from .views import FoodTypeViewset,CategoryMenuviewset
+from .views import FoodTypeViewset,CategoryMenuviewset, Orderviewset, SubCategoryMenuviewset, TableTypeviewset, Tableviewset
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('food-type',FoodTypeViewset,basename="food-type")
 router.register('category-menu',CategoryMenuviewset,basename="category-menu")
+router.register('subcategory-menu',SubCategoryMenuviewset,basename="subcategory-menu")
+router.register('table-type',TableTypeviewset,basename="table-type")
+router.register('table',Tableviewset,basename="table")
+router.register('order',Orderviewset,basename="order")
 
 urlpatterns = [
     path('',include(router.urls)),
